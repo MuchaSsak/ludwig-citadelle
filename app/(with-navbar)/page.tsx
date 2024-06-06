3a51754084c3d60ae21e09c790832451b1066ffe
MuchaSsak <MuchaSsak.tech+git@gmail.com>
2024-06-06 21:14:44 +0200

    fix: wrap home page in suspense boundary

--- app/(with-navbar)/page.tsx
+++ app/(with-navbar)/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import Quotes from "@/components/home-ui/Quotes";
 import ScrollToTop from "@/components/home-ui/ScrollToTop";
@@ -8,10 +8,12 @@ import Title from "@/components/home-ui/Title";
 function Home() {
   return (
     <main className="p-side mx-auto flex max-w-7xl flex-col items-center text-balance pb-32 pt-16 sm:pb-20">
-      <Title />
-      <Search />
-      <Quotes />
-      <ScrollToTop />
+      <Suspense>
+        <Title />
+        <Search />
+        <Quotes />
+        <ScrollToTop />
+      </Suspense>
     </main>
   );
 }
